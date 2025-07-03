6e53f7a8e4916d259b2db9da51ea6843998827d3
dipanjal <dipanjalmaitra@gmail.com>
2025-07-03 15:29:50 +0600

    fastapi: updated db env vars

--- fastapi-app/app/config/database.py
+++ fastapi-app/app/config/database.py
@@ -3,11 +3,25 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-if not DATABASE_URL:
-    raise RuntimeError("DATABASE_URL environment variable not set")
+db_config_map = {
+    "HOST": os.getenv("DB_HOST"),
+    "PORT": os.getenv("DB_PORT"),
+    "NAME": os.getenv("DB_NAME"),
+    "USER": os.getenv("DB_USER"),
+    "PASSWORD": os.getenv("DB_PASSWORD"),
+}
 
-engine = create_engine(DATABASE_URL, echo=False, pool_pre_ping=True)
+def get_db_url():
+    for key, value in db_config_map.items():
+        if not value:
+            raise RuntimeError(f"DB_{key} is not set")
+    return f"mysql+pymysql://{db_config_map['USER']}:{db_config_map['PASSWORD']}@{db_config_map['HOST']}:{db_config_map['PORT']}/{db_config_map['NAME']}"
+
+engine = create_engine(
+    get_db_url(),
+    echo=False,
+    pool_pre_ping=True
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
